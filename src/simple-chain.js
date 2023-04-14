0917d2a8fcfd45f0aa78da00694a9dbecb393a8a
Katerina Manzhak <59136296+manzhakkaterina@users.noreply.github.com>
2023-04-15 02:05:38 +0300

    Update simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,25 +4,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
+let mas = [];
 const chainMaker = {
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    return mas.length;
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(/* value */value) {
+    mas = mas + value;
     // remove line with error and write your code here
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(/* position */position) {
+      for (let i = 1; i < mas.length; i++) {
+    if (mas[i] === position) mas.splice(i, 1);
+  }
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    mas.reverse();
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+      for (let j = 0; j < mas.length - 1; j++) {
+    res = '( ' + mas[j] + ' )~~';
+  }
+  res = res + '( ' + mas[mas.length] + ' )';
+  console.log(res);
     // remove line with error and write your code here
   }
 };
