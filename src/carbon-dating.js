506d9c9dc787cfddbd53e7182a5a3f413c65b030
Katerina Manzhak <59136296+manzhakkaterina@users.noreply.github.com>
2023-04-14 15:33:24 +0300

    fix: fix mistake

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -24,7 +24,7 @@ if (sampleActivity < MODERN_ACTIVITY && sampleActivity > 0) {
 time = Math.ceil(Math.log((MODERN_ACTIVITY/sampleActivity))/(k1/HALF_LIFE_PERIOD));
 return time;
 } else return false;
-}
+
   // remove line with error and write your code here
 }
 
