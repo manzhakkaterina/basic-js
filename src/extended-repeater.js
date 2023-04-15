bd5c2ebabc4a2fa17249b5babe0c3b0cd8833f83
Katerina Manzhak <59136296+manzhakkaterina@users.noreply.github.com>
2023-04-15 17:09:27 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -21,11 +21,20 @@ var sep = '', sum = '';
 //var options = { repeatTimes: 0, separator: '', addition: '', additionRepeatTimes: 0, additionSeparator: '' };
 var res = '';
   if (options.additionSeparator === undefined) options.additionSeparator = '';
+  if (options.addition === undefined) options.addition = '';  
+  if (options.repeatTimes === undefined) options.repeatTimes = 0;
+  if (options.additionRepeatTimes === undefined) options.additionRepeatTimes = 0;
+  
   sep = options.addition + options.additionSeparator;
-  res = str + sep.repeat(options.additionRepeatTimes);
+  if (options.additionRepeatTimes === 0) res = str + sep;
+  else res = str + sep.repeat(options.additionRepeatTimes);
+  
   res = res.substr(0, res.length - options.additionSeparator.length);
   res = res + options.separator;
-  res = res.repeat(options.repeatTimes);
+  
+  if (options.repeatTimes !== 0) {
+  res = res.repeat(options.repeatTimes); }
+  
   res = res.substr(0, res.length - options.separator.length);
   return res;
   // remove line with error and write your code here
