c41127ab720d2a024d5acba4b790c4100a9b8292
Katerina Manzhak <59136296+manzhakkaterina@users.noreply.github.com>
2023-04-15 11:03:42 +0300

    Update simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,26 +4,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-  var mas = [];
-  var res = '';
+
 const chainMaker = {
+  mas: [],
   getLength() {
     return this.mas.length;
     // remove line with error and write your code here
   },
-  addLink(/* value */value = '') {
-    this.mas.push(value);
+  addLink(/* value */value) {
+    this.mas.push(value === undefined ? "( )" : `( ${value} )`);
     return this;
    // if (value !== null) mas = mas + value;
   //  else mas = mas + ' ';
     // remove line with error and write your code here
   },
   removeLink(/* position */position) {
-        if (typeof position != 'number' || position <= 0 || position > this.mas.length) {
+    const position1 = Number(position);
+    if (Number.isNaN(position1) || !Number.isInteger(position1)) {
       this.mas = [];
-      throw new Error("You can\'t remove incorrect link!");
+      throw new Error("You can't remove incorrect link!");
     }
-    this.mas.splice(position-1, 1);
+    const filt = this.mas.filter((x, i) => i != position1 - 1);
+    if (filt.length === this.mas.length) {
+      this.mas = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.values = filt;
     return this;
   //    for (let i = 0; i < mas.length; i++) {
   //  if (mas[i] === position) {mas.splice(i, 1); h++;}
@@ -31,18 +37,20 @@ const chainMaker = {
     // remove line with error and write your code here
   },
   reverseChain() {
-        this.mas = this.mas.reverse();
+    this.mas.reverse();
     return this;
- //   mas.reverse();
     // remove line with error and write your code here
   },
   finishChain() {
-    for (let i = 0; i < this.mas.length; i++) {
-      if (i == 0) res = '( ' + this.mas[0] + ' )';
-      else res += '~~( ' + this.mas[i] + ' )';
-    }
+    const res = this.mas.join("~~");
     this.mas = [];
     return res;
+  //  for (let i = 0; i < this.mas.length; i++) {
+   //   if (i == 0) res = '( ' + this.mas[0] + ' )';
+  //    else res += '~~( ' + this.mas[i] + ' )';
+   // }
+  //  this.mas = [];
+  //  return res;
   //    for (let j = 0; j < mas.length - 1; j++) {
   //  res = '( ' + mas[j] + ' )~~';
   //}
