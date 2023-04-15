9205c78684bb60db12fcc35584fd34d42b359d5e
Katerina Manzhak <59136296+manzhakkaterina@users.noreply.github.com>
2023-04-15 16:50:57 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,8 +15,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(/* str, options */str, options) {
+//var str = '';
+var sep = '', sum = '';
+//var options = { repeatTimes: 0, separator: '', addition: '', additionRepeatTimes: 0, additionSeparator: '' };
+var res = '';
+  if (options.additionSeparator === undefined) options.additionSeparator = '';
+  sep = options.addition + options.additionSeparator;
+  res = str + sep.repeat(options.additionRepeatTimes);
+  res = res.substr(0, res.length - options.additionSeparator.length);
+  res = res + options.separator;
+  res = res.repeat(options.repeatTimes);
+  res = res.substr(0, res.length - options.separator.length);
+  return res;
   // remove line with error and write your code here
 }
 
